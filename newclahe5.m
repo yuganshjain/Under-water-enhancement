2aa9a0682669a89ec754e2f45d48461016447c97
Yugansh jain <47623725+yuganshjain@users.noreply.github.com>
2023-03-03 10:02:51 +0530

    Create newclahe5.m
    adding fusion methods

---
+++ newclahe5.m
@@ -0,0 +1,72 @@
+close all
+clear all
+clc
+
+%% Load image
+RGB_img = imread('underwater_fish.JPG');
+
+%% Apply CLAHE to enhance contrast
+hsv_img = rgb2hsv(RGB_img);
+L = hsv_img; 
+L(:,:,3) = adapthisteq(L(:,:,3),'NumTiles', [10 10],'ClipLimit',0.05);
+RGB_img_clahe = hsv2rgb(L);
+
+%% Apply color correction using gray world assumption
+% Calculate average R, G, B values
+R = RGB_img_clahe(:,:,1);
+G = RGB_img_clahe(:,:,2);
+B = RGB_img_clahe(:,:,3);
+avgR = mean2(R);
+avgG = mean2(G);
+avgB = mean2(B);
+
+% Find scaling factors
+kR = avgG / avgR;
+kB = avgG / avgB;
+
+% Apply scaling factors to R and B channels
+R_corrected = R * kR;
+B_corrected = B * kB;
+
+% Recombine channels
+RGB_img_corrected = cat(3, R_corrected, G, B_corrected);
+
+%% Apply fusion-based technique to further enhance image
+gray_img = rgb2gray(RGB_img_corrected);
+edge_img = edge(gray_img, 'canny', 0.3);
+
+% Create high-pass filter
+highpass_filter = [0 -1 0; -1 4 -1; 0 -1 0];
+
+% Apply filter to edge image
+edge_filtered = imfilter(double(edge_img), highpass_filter);
+
+% Add filtered edge image to original image
+RGB_img_fused = RGB_img_corrected;
+for i = 1:3
+    RGB_img_fused(:,:,i) = RGB_img_corrected(:,:,i) + edge_filtered;
+end
+
+%% Display results
+figure, imshow(RGB_img);
+title('Original image');
+
+figure, imshow(RGB_img_clahe);
+title('CLAHE enhanced image');
+
+figure, imshow(RGB_img_corrected);
+title('Color corrected image');
+
+figure, imshow(RGB_img_fused);
+title('Fused image');
+
+%% Function to compare results with original image
+function oldresults(original, new)
+    figure;
+    subplot(1,2,1);
+    imshow(original);
+    title('Original image');
+    subplot(1,2,2);
+    imshow(new);
+    title('Enhanced image');
+end
