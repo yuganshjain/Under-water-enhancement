38cfe0f7626f5f2d1d140c192cf9067085121f7c
Yugansh jain <47623725+yuganshjain@users.noreply.github.com>
2023-05-24 12:09:14 +0530

    Update uicm2.m

--- uicm2.m
+++ uicm2.m
@@ -1,41 +1,31 @@
 % Read the underwater image
-image = imread('wav.jpg');
+image = imread('1.jpg');
 
 % Convert the image to double precision
 image = im2double(image);
 
+% Extract the color channels (R, G, B)
+R = image(:,:,1);
+G = image(:,:,2);
+B = image(:,:,3);
+
 % Calculate the RG and YB color components
-RG = image(:,:,1) - image(:,:,2);
-YB = (image(:,:,1) + image(:,:,2)) / 2 - image(:,:,3);
+RG = R - G;
+YB = (R + G) / 2 - B;
 
 % Flatten the color components into a 1D array
 RG = RG(:);
 YB = YB(:);
 
-% Sort the color component values
-sortedRG = sort(RG);
-sortedYB = sort(YB);
+% Calculate the mean and variance of the color components
+meanRG = mean(RG);
+varRG = var(RG);
+meanYB = mean(YB);
+varYB = var(YB);
 
-% Define the trimming percentage
-alphaL = 0.05; % Trimming percentage for lower values
-alphaR = 0.05; % Trimming percentage for higher values
-
-% Calculate the number of values to be trimmed
-N = numel(RG);
-TL = floor(alphaL * N);
-TR = floor(alphaR * N);
-
-% Trim the color component values
-trimmedRG = sortedRG(TL+1 : end-TR);
-trimmedYB = sortedYB(TL+1 : end-TR);
-
-% Calculate the mean and variance of the trimmed color components
-meanRG = mean(trimmedRG);
-varRG = var(trimmedRG);
-meanYB = mean(trimmedYB);
-varYB = var(trimmedYB);
 % Calculate the UICM score
-UICM = -0.0268 * sqrt(meanRG^2) + sqrt(meanYB^2) + 0.1586 * sqrt(varRG^2) + sqrt(varYB^2);
+UICM = -0.0268 * sqrt(meanRG^2 + meanYB^2) + 0.1586 * sqrt(varRG^2 + varYB^2);
+
 
 % Display the UICM results
 disp(['UICM - RG Mean: ' num2str(meanRG)]);
